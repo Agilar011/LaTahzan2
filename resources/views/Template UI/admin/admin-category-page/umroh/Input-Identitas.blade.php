ca932da6488a7144dcc10599f17f4b91f27971fb
Agilar Gumilar <88954314+Agilar011@users.noreply.github.com>
2023-08-10 11:52:22 +0700

    Update Input-Identitas.blade.php

--- resources/views/Template UI/admin/admin-category-page/umroh/Input-Identitas.blade.php
+++ resources/views/Template UI/admin/admin-category-page/umroh/Input-Identitas.blade.php
@@ -16,43 +16,8 @@
                 <label for="id_etalase_umroh"></label>
                 <input type="text" name="id_etalase_umroh" value="{{ $data->id }}" disabled>
                 <label for="">Data Jemaah</label>
-                {{-- <div class="form-input">
-                    <label for="nama_jemaah{{ 1 }}">Nama Jemaah {{ 1 }}</label>
-                    <input type="text" name="nama_jemaah[]" id="nama_jemaah{{ $user->name }}" required>
-
-                    <label for="NIK{{ 1 }}">NIK Jemaah {{ 1 }}</label>
-                    <input type="text" name="NIK[]" id="NIK{{ $user->nik }}" required>
-
-                    <label for="No_hp{{ 1 }}">No HP Jemaah {{ 1 }}</label>
-                    <input type="text" name="No_hp[]" id="No_hp{{ $user->phone }}" required>
-
-                    <label for="no_paspor{{ 1 }}">No Paspor {{ 1 }}</label>
-                    <input type="text" name="no_paspor[]" id="no_paspor{{ 1 }}" required>
-
-                    <label for="foto_paspor{{ 1 }}">Foto Paspor {{ 1 }}</label>
-                    <input type="file" name="foto_paspor[]" id="foto_paspor{{ 1 }}" required>
-
-                    <label for="foto_KTP{{ 1 }}">Foto KTP {{ 1 }}</label>
-                    <input type="file" name="foto_KTP[]" id="foto_KTP{{ 1 }}" required>
-
-                    <select class="status" name="status_vaksin">
-                        <option selected>Pilih Status Vaksin Anda</option>
-                        <option value="Sudah">Sudah</option>
-                        <option value="Belum">Belum</option>
-                    </select>
-                    <label for="foto_vaksin{{ 1 }}">Foto Vaksin {{ 1 }}</label>
-                    <input type="file" name="foto_vaksin[]" id="foto_vaksin{{ 1 }}" required>
-                    <!-- Include other input fields for jemaah data (foto_paspor, foto_KTP, etc.) -->
-                </div> --}}
-
                 @if ($data->jumlah_jemaah > 0)
                 @for ($i = 1; $i <= $data->jumlah_jemaah; $i++)
-                @if ()
-
-                @else
-
-                @endif
-                if
                     <div class="form-input">
                         <label for="nama_jemaah{{ $i }}">Nama Jemaah {{ $i }}</label>
                         <input type="text" name="nama_jemaah[]" id="nama_jemaah{{ $i }}" required>
